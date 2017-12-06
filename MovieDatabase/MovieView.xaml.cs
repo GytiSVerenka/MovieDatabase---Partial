384a32110782d19eb86d026c1dd22618692320ea
Gytis Verenka <csf16veg@SCIS.ULSTER.AC.UK>
2017-12-06 16:18:21 +0000

    Add project files.

---
+++ MovieDatabase/MovieView.xaml.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.Win32;
+using MovieDatabase.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MovieDatabase
+{
+    /// <summary>
+    /// Interaction logic for MovieView.xaml
+    /// </summary>
+    public partial class MovieView : Window
+    {
+     
+        public MovieView()
+        {
+            InitializeComponent();
+        }
+
+        private void FileMenu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FileNewMenu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FileOpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FileSaveMenu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FileExitMenu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EditCreateMenu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EditEditMenu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EditDeleteMenu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ViewByTitle_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ViewByYear_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ViewByDuration_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void HelpAbout_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BFirst_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BPrevious_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BNext_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BLast_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Create what ever methods you deem necessary to provide a functioning UI
+
+    }
+}
